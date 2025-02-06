874558db4dfa8bddc25f9888c6abe55d3d024e85
Sierra555 <dara6234@gmail.com>
2025-02-06 21:22:01 +0200

    upda path to dist

--- server.cjs
+++ server.cjs
@@ -9,11 +9,10 @@ const axios = require('axios');
 
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, '../dist')));
-
+app.use(express.static(path.join(__dirname, './dist')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../dist', 'index.html'));
+  res.sendFile(path.resolve(__dirname, './dist', 'index.html'));
 });
 
 app.post('/generate-task', async (request, response) => {
